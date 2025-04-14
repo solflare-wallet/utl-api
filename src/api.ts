78a2824339657ad19ff4adb2471f31ab168600eb
MicicFilip <micic.filip@protonmail.com>
2025-04-14 11:54:01 +0200

    Add mongoose disconnect handler

--- src/api.ts
+++ src/api.ts
@@ -5,14 +5,24 @@ dotenv.config()
 import ApiSetup from './api/setup'
 
 mongoose
-    .connect(process.env.DB_URL as string)
-    .then((db) => {
-        console.log(`Connected to ${db.connections[0].name} - mongodb`)
-        ApiSetup.listen(process.env.PORT ?? 80, () => {
-            console.log(`Express running on port ${process.env.PORT}`)
-        })
+  .connect(process.env.DB_URL as string)
+  .then((db) => {
+    console.log(`Connected to ${db.connections[0].name} - mongodb`)
+    ApiSetup.listen(process.env.PORT ?? 80, () => {
+      console.log(`Express running on port ${process.env.PORT}`)
     })
-    .catch((error) => {
-        console.log('There was an error connecting to db')
-        console.log(error)
-    })
+  })
+  .catch((error) => {
+    console.log('There was an error connecting to db')
+    console.log(error)
+  })
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected!');
+});
+
+process.on('SIGTERM', () => {
+  mongoose.disconnect().then(() => {
+    console.log('Mongoose disconnected!');
+  });
+});
